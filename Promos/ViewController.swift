a843887a3fc9888a79265de16c86940c1008de8e
Imad Ajallal <Dopest@Imads-MacBook-Pro.local>
2016-12-23 10:52:19 +0000

    Initial Commit

---
+++ Promos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Promos
+//
+//  Created by Imad Ajallal on 12/23/16.
+//  Copyright © 2016 Imad Ajallal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
